15e6cbe44310df2deb0867855fec3706ab62944e
qiberry <nikitaatkachenko@gmail.com>
2022-05-29 15:21:01 +0300

    Added the ability to upload copies of files

--- upload_api.php
+++ upload_api.php
@@ -8,20 +8,20 @@ include 'dbconfig.php'; // include database connection file
 
 $data = json_decode(file_get_contents("php://input"), true); // collect input parameters and convert into readable format
 	
-$fileName  =  $_FILES['sendimage']['name'];
-$tempPath  =  $_FILES['sendimage']['tmp_name'];
-$fileSize  =  $_FILES['sendimage']['size'];
+$fileName  =  $_FILES['sendfile']['name'];
+$tempPath  =  $_FILES['sendfile']['tmp_name'];
+$fileSize  =  $_FILES['sendfile']['size'];
 		
 if(empty($fileName))
 {
-	$errorMSG = json_encode(array("message" => "please select text file", "status" => false));	
+	$errorMSG = json_encode(array("message" => "please select image", "status" => false));	
 	echo $errorMSG;
 }
 else
 {
 	$upload_path = 'upload/'; // set upload folder path 
 	
-	$fileExt = strtolower(pathinfo($fileName,PATHINFO_EXTENSION)); // get file extension
+	$fileExt = strtolower(pathinfo($fileName,PATHINFO_EXTENSION)); // get image extension
 		
 	// valid image extensions
 	$valid_extensions = array('txt'); 
@@ -30,26 +30,25 @@ else
 	if(in_array($fileExt, $valid_extensions))
 	{				
 		//check file not exist our upload folder path
-		if(!file_exists($upload_path . $fileName))
+		while(file_exists($upload_path . $fileName))
 		{
-			// check file size '5MB'
-			if($fileSize < 5000000){
-				move_uploaded_file($tempPath, $upload_path . $fileName); // move file from system temporary path to our upload folder path 
-			}
-			else{		
-				$errorMSG = json_encode(array("message" => "Sorry, your file is too large, please upload 5 MB size", "status" => false));	
-				echo $errorMSG;
-			}
+			//making new filename for file
+			$pos = strlen($fileName) - 4;
+			$fileName = substr_replace($fileName, "_copy", $pos, 0);
 		}
-		else
-		{		
-			$errorMSG = json_encode(array("message" => "Sorry, file already exists check upload folder", "status" => false));	
+
+		// check file size '5MB'
+		if($fileSize < 5000000){
+			move_uploaded_file($tempPath, $upload_path . $fileName); // move file from system temporary path to our upload folder path 
+		}
+		else{		
+			$errorMSG = json_encode(array("message" => "Sorry, your file is too large, please upload 5 MB size", "status" => false));	
 			echo $errorMSG;
 		}
 	}
 	else
 	{		
-		$errorMSG = json_encode(array("message" => "Sorry, only TXT files are allowed", "status" => false));	
+		$errorMSG = json_encode(array("message" => "Sorry, only JPG, JPEG, PNG & GIF files are allowed", "status" => false));	
 		echo $errorMSG;		
 	}
 }
@@ -59,7 +58,7 @@ if(!isset($errorMSG))
 {
 	$query =  mysqli_query($conn,'INSERT into tbl_image (name) VALUES("'.$fileName.'")');
 			
-	echo json_encode(array("message" => "File Uploaded Successfully", "status" => true));	
+	echo json_encode(array("message" => "Image Uploaded Successfully", "status" => true));	
 }
 
 ?>
